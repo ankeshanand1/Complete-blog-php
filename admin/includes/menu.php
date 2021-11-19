946fb9cc2d5188c4cf204a8050a791603c8491fe
Ankesh Anand <ankesh@cloudmate.in>
2021-11-20 00:33:57 +0530

    Update menu.php

--- admin/includes/menu.php
+++ admin/includes/menu.php
@@ -4,10 +4,10 @@
 			<h2>Actions</h2>
 		</div>
 		<div class="card-content">
-			<a href="<?php echo BASE_URL . 'admin/create_post.php' ?>">Create Posts</a>
-			<a href="<?php echo BASE_URL . 'admin/posts.php' ?>">Manage Posts</a>
-			<a href="<?php echo BASE_URL . 'admin/users.php' ?>">Manage Users</a>
-			<a href="<?php echo BASE_URL . 'admin/topics.php' ?>">Manage Topics</a>
+			<a href="<?php echo BASE_URL . 'create_post.php' ?>">Create Posts</a>
+			<a href="<?php echo BASE_URL . 'posts.php' ?>">Manage Posts</a>
+			<a href="<?php echo BASE_URL . 'users.php' ?>">Manage Users</a>
+			<a href="<?php echo BASE_URL . 'topics.php' ?>">Manage Topics</a>
 		</div>
 	</div>
 </div>
