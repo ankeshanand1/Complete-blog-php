9f8bcc11e90d31368fa6347ae6ac5a7439e7ee31
Ankesh Anand <ankesh@cloudmate.in>
2021-11-20 00:00:53 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php 
 	session_start();
 	// connect to database
-	$conn = mysqli_connect("localhost", "root", "", "complete-blog-php");
+	$conn = mysqli_connect("localhost", "USERNAME", "PASSWORD", "DATABASE_NAME");
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
 	}
     // define global constants
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	define('BASE_URL', 'http://localhost/complete-blog-php/');
+	define('BASE_URL', "https://$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']");
 ?>
